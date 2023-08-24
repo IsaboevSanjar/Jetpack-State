45f8b300238c56f695f08224a4ccc5fbbae6bdcb
IsaboevSanjar <sanjarisaboev@yandex.com>
2023-08-24 16:53:55 +0500

    State saved

--- app/src/main/java/sanjar/uz/jetpacksatate/WaterCounter.kt
+++ app/src/main/java/sanjar/uz/jetpacksatate/WaterCounter.kt
@@ -5,6 +5,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -12,7 +17,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+    var count by rememberSaveable { mutableStateOf(0) }
     Column(modifier = modifier.padding(16.dp)) {
         Text(text = "You've had $count glasses")
         Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp)) {
