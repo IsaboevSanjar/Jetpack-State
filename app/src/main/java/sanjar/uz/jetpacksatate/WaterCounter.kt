10dd8e66f2c07b9fa13aa745923f1fca2ce2e09f
IsaboevSanjar <sanjarisaboev@yandex.com>
2023-08-24 17:24:40 +0500

    State hoisting

--- app/src/main/java/sanjar/uz/jetpacksatate/WaterCounter.kt
+++ app/src/main/java/sanjar/uz/jetpacksatate/WaterCounter.kt
@@ -1,6 +1,7 @@
 package sanjar.uz.jetpacksatate
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -20,15 +21,30 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     var count by rememberSaveable { mutableStateOf(0) }
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
+            var showTask by rememberSaveable { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false })
+            }
             Text(text = "You've had $count glasses")
         }
-        Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
-            Text(text = "Add one")
+        Row(modifier = Modifier.padding(8.dp)) {
+            Button(
+                onClick = { count++ },
+                modifier = Modifier.padding(top = 8.dp),
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp, top = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
         }
+
     }
 
 }
