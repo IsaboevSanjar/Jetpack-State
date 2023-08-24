b0d7fecdf778f44f86f0bed0d6ed4a5467e4f889
IsaboevSanjar <sanjarisaboev@yandex.com>
2023-08-24 16:28:40 +0500

    First Set Up

--- app/src/main/java/sanjar/uz/jetpacksatate/MainActivity.kt
+++ app/src/main/java/sanjar/uz/jetpacksatate/MainActivity.kt
@@ -22,25 +22,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    WellnessScreen(modifier = Modifier.fillMaxSize())
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackSatateTheme {
-        Greeting("Android")
     }
 }
